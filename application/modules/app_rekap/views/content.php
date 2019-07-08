985629a4e9838c3e7425217b0501ab644e4f09b1
Yudi Wahyudi <yudi.wahyudi154@gmail.com>
2019-07-08 17:26:06 +0700

    update api event

---
+++ application/modules/app_rekap/views/content.php
@@ -0,0 +1,66 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Dashboard
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section id="content-main" class="content">
+
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="row">
+                        <!-- /.col -->
+
+                        <div class="col-md-4">
+                            <select id="satker" name="satker" class="form-control"></select>
+                        </div>
+                        <div class="col-md-8">
+                            <select id="riset" name="riset" class="form-control"></select>
+
+                        </div>
+                        <div id="labelriset" class="col-md-12" style="padding-top: 20px">
+                            <p class="text-center">
+                                <strong>Riset</strong>
+                            </p>
+                        </div>
+                        <div id="chart" class="col-md-12">
+
+<!--                            <div class="progress-group">-->
+<!--                                <span class="progress-text">Add Products to Cart</span>-->
+<!--                                <span class="progress-number"><b>160</b>/200</span>-->
+<!---->
+<!--                                <div class="progress sm">-->
+<!--                                    <div class="progress-bar progress-bar-aqua" style="width: 80%"></div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+
+                            <!-- /.progress-group -->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- ./box-body -->
+                <!-- /.box-footer -->
+            </div>
+            <!-- /.box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+
+
+</section>
+
+<!-- JS content -->
+<script src="<?php echo base_url() . 'assets/modules/app_dashboard/dashboard-content.js' ?>"></script>
