c762b90d3d356f51c2ba958920aefa603253f46d
Yudi Wahyudi <yudi.wahyudi154@gmail.com>
2019-08-01 19:41:07 +0700

    update api grafik

--- application/modules/api_v1/models/Api_v1_model.php
+++ application/modules/api_v1/models/Api_v1_model.php
@@ -164,12 +164,12 @@ class Api_v1_model extends CI_Model
         if (is_null($data["id_event"])) {
             return result(new stdClass(), 201, "Event not found...!");
         } else {
-            $sql = "select z.id_event,z.event,z.tanggal,z.id_rdg,z.nama_rdg,z.id_satker,z.kode_satker,z.satker,avg(z.value) as value_avg
+            $sql = "select z.id_event,z.event,z.tanggal,z.id_rdg,z.nama_rdg,z.id_satker,z.kode_satker,z.satker,z.value as value_avg
 						from
 						(
 						select a.id_event,a.event,a.tanggal,a.end_periode,b.id_rdg,
 							   c.nama_rdg,c.id_satker,d.kode_satker,d.satker,c.id_matrix,e.matrix_table,c.tanggal tanggal_rdg,
-							   f.id_aspek,g.id_parent,g.aspek,sum(ifnull(cast(ifnull(h.value,0) as int),0)) as value
+							   f.id_aspek,g.id_parent,g.aspek,avg(ifnull(cast(ifnull(h.value,0) as int),0)) as value
 						 from ref_event a left join ref_event_rdg b on a.id_event=b.id_event
 							  left join ref_rdg c on b.id_rdg=c.id_rdg
 							  left join ref_satuan_kerja d on c.id_satker=d.id_satker
@@ -204,12 +204,12 @@ class Api_v1_model extends CI_Model
 
         } else if ($data["id_event"] and $data["id_rdg"]) {
             $sql = "select z.id_event,z.event,z.tanggal,z.id_rdg,z.nama_rdg,z.id_satker,z.kode_satker,z.satker, z.id_parent, z.id_matrix, y.id_aspek, x.aspek,
-						   avg(z.value) as value_avg
+						   z.value as value_avg
 					from
 					(
 					select a.id_event,a.event,a.tanggal,a.end_periode,b.id_rdg,
 						   c.nama_rdg,c.id_satker,d.kode_satker,d.satker,c.id_matrix,e.matrix_table,c.tanggal tanggal_rdg,
-						   f.id_aspek,if(g.id_parent is null or g.id_parent=0,g.id_aspek,g.id_parent) id_parent,g.aspek,sum(ifnull(cast(ifnull(h.value,0) as int),0)) as value
+						   f.id_aspek,if(g.id_parent is null or g.id_parent=0,g.id_aspek,g.id_parent) id_parent,g.aspek,avg(ifnull(cast(ifnull(h.value,0) as int),0)) as value
 					 from ref_event a left join ref_event_rdg b on a.id_event=b.id_event
 						  left join ref_rdg c on b.id_rdg=c.id_rdg
 						  left join ref_satuan_kerja d on c.id_satker=d.id_satker
