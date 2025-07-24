56382b4ed84a1ff953250b48e83f1ab933dce74a
yudiwahyudi154 <yudi.wahyudi154@gmail.com>
2025-07-24 21:45:10 +0700

    hide tombol download productivity

--- application/modules/rep_productivity/views/form.php
+++ application/modules/rep_productivity/views/form.php
@@ -119,7 +119,7 @@
 
                     <div class="box-footer">
                         <button id="btn-preview-form" type="button" class="btn btn-success fa fa-book"> View</button>
-                        <button id="btn-download-form" type="button" class="btn btn-primary fa fa-download">  Download</button>
+                        <!--<button id="btn-download-form" type="button" class="btn btn-primary fa fa-download">  Download</button>-->
                         <button id="btn-download-all-data-form" type="button" class="btn btn-primary fa fa-download">  Download Data Visit & Order</button>
                     </div>
                     <div class="row">
