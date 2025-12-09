7798e6398d73b298bc9ace5a2258479dc9e3fdc9
Yusuf Hartanto <yusuf.hartanto69@gmail.com>
2025-12-09 16:08:07 +0700

    update:dashboard - chart (productivity, performance, presensi, man power)

--- application/modules/dashboard_chart/views/content.php
+++ application/modules/dashboard_chart/views/content.php
@@ -1,12 +1,24 @@
 <style>
     .chart-container {
-        height: 330px;
+        position: relative;
+        width: 100%;
+        height: 350px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    #chartParmaCoverage {
+        max-width: 100% !important;
+        max-height: 100% !important;
     }
     .dashboard-card {
         padding: 20px;
         background: white;
         border-radius: 10px;
         box-shadow: 0 2px 6px rgba(0,0,0,0.1);
+    }
+    .mt-3r {
+        margin-top: 3rem;
     }
 </style>
 
@@ -24,27 +36,95 @@
 <!-- Main content -->
 <section id="content-main" class="content">
     <div class="row">
-        <div class="col-xs-6">
-            <h4 class="section-title">Report Productivity</h4>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <div class="form-group col-md-3">
+                        <label for="periode">Periode</label>
+                        <select id="periode_id" name="periode" class="form-control" placeholder="Select Periode">
+                            <option value="today" selected>Hari Ini</option>
+                            <option value="week">Minggu Ini</option>
+                            <option value="month">Bulan Ini</option>
+                            <option value="year">Tahun Ini</option>
+                            <option value="custom">Pilih Tanggal</option>
+                        </select>
+                    </div>
+                    <div id="date_range" class="hidden">
+                        <div class="form-group col-md-2">
+                            <label for="start_periode">Start Periode</label>
+                            <div class="input-group date">
+                                <div class="input-group-addon">
+                                    <span class="glyphicon glyphicon-th"></span>
+                                </div>
+                                <input id="start_periode" placeholder="Periode" type="text" class="form-control datepicker" name="start_periode" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-2">
+                            <label for="end_periode">End Periode</label>
+                            <div class="input-group date">
+                                <div class="input-group-addon">
+                                    <span class="glyphicon glyphicon-th"></span>
+                                </div>
+                                <input id="end_periode" placeholder="End Periode" type="text" class="form-control datepicker" name="end_periode" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <button
+                            id="btn_load_data"
+                            type="button"
+                            class="btn btn-warning fa fa-refresh"
+                            style="margin-top: 2.7rem;"
+                        > Load Data</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <h4 class="section-title">
+                Report Productivity
+                <span id="labelProductivity"></span>
+            </h4>
             <div class="dashboard-card mb-4">
                 <div class="chart-container">
                     <canvas id="chartProductivity"></canvas>
                 </div>
             </div>
         </div>
-        <div class="col-xs-6">
-            <h4 class="section-title">Performa PAR</h4>
+        <div class="col-md-6 col-sm-12">
+            <h4 class="section-title">
+                Performance PAR-MA
+                <span id="labelPerformance"></span>
+            </h4>
             <div class="dashboard-card mb-4">
                 <div class="chart-container">
                     <canvas id="chartPerformance"></canvas>
                 </div>
             </div>
         </div>
-        <div class="col-xs-6">
-            <h4 class="section-title">Summary Man Power</h4>
+    </div>
+    <div class="row mt-3r">
+        <div class="col-md-6 col-sm-12">
+            <h4 class="section-title">
+                Preseni PAR-MA
+                <span id="labelPresensi"></span>
+            </h4>
             <div class="dashboard-card mb-4">
                 <div class="chart-container">
-                    <canvas id="chartSummary"></canvas>
+                    <canvas id="chartPresensi"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <h4 class="section-title">
+                Man Power PAR-MA
+                <span id="labelParmaCoverage"></span>
+            </h4>
+            <div class="dashboard-card mb-4">
+                <div class="chart-container">
+                    <canvas id="chartParmaCoverage"></canvas>
                 </div>
             </div>
         </div>
